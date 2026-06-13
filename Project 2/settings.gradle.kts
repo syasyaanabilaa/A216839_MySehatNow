a8149b0e5756e992b08f5b471bfdae9fe0b6890d
syasya nabilah <a216839@siswa.ukm.edu.my>
2026-06-13 19:13:01 +0800

    Project 2

---
+++ Project 2/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "A216839_Wan_Project2"
+include(":app")
